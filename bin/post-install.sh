c7423b2101f7d3aa57c8a2fb5614aef56d938a04
Michael Vu <email@michael.vu>
2023-04-08 04:40:22 +0100

    adding features

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash                                                                         
+mvu docker --enable port80
+mvu docker --enable site-name
 mvu docker --enable kidzartz-settings
